9623197d5f8d5e292b2fd75efcc664a277e9970e
ramz <ramz.13m@mail.ru>
2022-05-11 03:33:14 +0500

    clear js code

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 const chk = document.getElementById('chk');
 
-chk.addEventListener('change', () => {
+chk.addEventListener('click', () => {
 	document.body.classList.toggle('dark');
 });
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
